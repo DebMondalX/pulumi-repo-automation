f4cdfe1f0e1ac49a4efef92c4e65210312e61f65
Debarghya Mondal <debarghya@Debarghyas-MacBook-Air.local>
2025-04-05 03:35:36 +0530

    minor modification

--- __main__.py
+++ __main__.py
@@ -1,20 +1,6 @@
 import pulumi
-import pulumi_github as github
+from repo import repo
 
-# Read config values from Pulumi Automation API
-config = pulumi.Config()
-repo_name = config.require("repoName")
-repo_description = config.require("repoDescription")
-is_private = config.get_bool("repoPrivate") or False
-
-# Create the repository
-repo = github.Repository(repo_name,
-    name=repo_name,
-    auto_init=True,
-    description=repo_description,
-    visibility="private" if is_private else "public"
-)
-
-# Export repo metadata
+# Export the repository name and URL
 pulumi.export("repo_name", repo.name)
 pulumi.export("repo_url", repo.html_url)
